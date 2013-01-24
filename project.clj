15cd9f29b41564e6fe6ee7e759f322e8011ddb42
Joel Boehland <joel.boehland@evocomputing.com>
2013-01-24 11:46:38 -0800

    Add namespace tools for REPL

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject dataspace "0.1.0-SNAPSHOT"
-  :description "N Dimensional array abstractions. Heavily influenced by Chapele language and the python Blaze projects"
+  :description "N Dimensional array abstractions. 
+Heavily influenced by Chapele language and the python Blaze projects"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.namespace "0.2.2"]])
